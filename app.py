59494eba5f42d559ffb9b214c5f7e5cc7896835b
marioliveira-mb <mariana.oliveira@mb.com.br>
2024-02-23 14:30:55 -0300

    Implementando herança

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from models.restaurante import Restaurante
+from models.cardapio.bebida import Bebida
+from models.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Mariana', 10)
-restaurante_praca.receber_avaliacao('Mari', 2)
-
-# restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
-# restaurante_japones = Restaurante('Japa', 'Japonesa')
-
-# restaurante_mexicano.alternar_estado()
+bebida_suco = Bebida('Suco de melancia', 5.0, 'grande')
+prato_pao = Prato('Pãozinho', 2.0, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_pao)
 
 if __name__ == '__main__':
     main()
